010f8b12c625dee2d659300d2e9c5160a29e0125
Ayoub BENABID <ayoub.benabid@smarteez.eu>
2024-03-06 20:31:41 +0100

    Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Box, Text, Heading, SimpleGrid, Button, Flex, Divider, Link } from "@chakra-ui/react"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <SimpleGrid columns={{ sm: 1, lg: 2 }} spacing={10}>
+      <Box>
+        <Heading fontSize="48px" as="h1" mb={4}>Your Path to Resolution</Heading>
+        <Text mb={4}>
+          You’ll start by answering a series of questions, such as the length of your marriage, if you have children together, and if there is property or debt to divide.So that your case gets the appropriate level of attention.
+        </Text>
+        <Text mb={4}>
+          Your answers will not be shared with the opposing party or attorney. This form is not considered to be a public record.
+        </Text>
+        <Text mb={8}>
+          These questions identify support and resources available for your family, such as legal assistance, mediation or judicial supervision.
+        </Text>
+        <Link bg="#008992" color="#FFFFFF" _hover={{ bg: "#008992DD" }} px={6} py={3} borderRadius="10px" href="/interview">
+          Start
+        </Link>
+      </Box>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Box>
+        <Box bg="#FFCD0017" border={"3px solid #FFCD00"} p="32px" borderRadius="10px">
+          <Flex gap={4} p="12px" alignItems="flex-start">
+            <Image src="/images/book-icon.svg" alt="" width={21} height={26} />
+            <Box>
+              <Heading fontSize="20px" as="h2">Answer questions</Heading>
+              <Text fontSize="18px">
+                You will complete a short questionnaire to help determine the best path for your case. You will only answer questions relevant for you, so it’s easy and fast.
+              </Text>
+            </Box>
+          </Flex>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <Divider my={4} borderColor="#2D3648" borderBottomWidth="2px" />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <Flex gap={4} p="12px" alignItems="flex-start">
+            <Image src="/images/book-icon.svg" alt="" width={21} height={26} />
+            <Box>
+              <Heading fontSize="20px" as="h2">Complete Court Forms</Heading>
+              <Text fontSize="18px">
+                The portal will use your answers to complete the court forms you need to submit.
+              </Text>
+            </Box>
+          </Flex>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+          <Divider my={4} borderColor="#2D3648" borderBottomWidth="2px" />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+          <Flex gap={4} p="12px" alignItems="flex-start">
+            <Image src="/images/book-icon.svg" alt="" width={21} height={26} />
+            <Box>
+              <Heading fontSize="20px" as="h2">File Your Documents</Heading>
+              <Text fontSize="18px">
+                You can then file your forms directly with the court through the portal. Or you can download, print, and submit them in person at the courthouse.
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
+    </SimpleGrid>
+  )
 }
