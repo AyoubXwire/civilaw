07a9ae20e36da2acd79f3b35499a4a3f1363bca7
Ayoub <benayoubid@gmail.com>
2024-03-11 18:18:59 +0000

    Implement progress bar logic

--- src/app/_components/ProgressBar.tsx
+++ src/app/_components/ProgressBar.tsx
@@ -1,28 +1,56 @@
 "use client"
 
 import useInterview from "@/features/interview"
-import { Box, Step, StepIcon, StepIndicator, StepNumber, StepSeparator, StepStatus, StepTitle, Stepper } from "@chakra-ui/react"
+import { Box, Step, StepIndicator, StepSeparator, StepStatus, StepTitle, Stepper } from "@chakra-ui/react"
+import { useEffect } from "react"
 
 export default function ProgressBar() {
 
   const {
-    progress: {
-      steps,
-      visibleStep,
-      setVisibleStep,
-    },
-  } = useInterview()
+    steps,
+    visibleStep,
+    currentStep,
+    setVisibleStep,
+    setCurrentStep,
+  } = useInterview(state => state.progress)
+
+  const data = useInterview(state => state.data)
+
+  // Update current step depending on the data
+  useEffect(() => {
+    if (visibleStep === 0) {
+      if (data.livedInNevadaForAtLeast6Weeks === true) return setCurrentStep(1)
+      if (data.livedInNevadaForAtLeast6Weeks === false) return setCurrentStep(0)
+    }
+    else if (visibleStep === 1) {
+      if (data.propertyOrAssetsToDivide === false) return setCurrentStep(2)
+      if (
+        data.propertyOrAssetsToDivide === true
+        && data.agreeOnHowToDivideProperty !== undefined
+        && data.agreeOnDivisionOfAssets !== undefined
+        && data.agreeOnDivisionOfDepts !== undefined
+        && data.agreeOnFinancialSupport !== undefined
+      ) return setCurrentStep(2)
+    }
+  }, [data, setCurrentStep])
+
+  const stepColor = (index: number) => {
+    if (currentStep === index) return "blue.500"
+    if (visibleStep === index) return "white"
+    return "gray.200"
+  }
 
   return (
     <Stepper index={visibleStep}>
       {steps.map((stepName, index) => (
         <Step key={index}>
           <StepIndicator onClick={() => setVisibleStep(index)} cursor="pointer">
-            <StepStatus
-              complete={<Box borderRadius="full" border={"2px solid blue.500"} w="100%" h="100%"></Box>}
-              incomplete={<Box borderRadius="full" border={"2px solid blue.500"} w="100%" h="100%"></Box>}
-              active={<Box bg="blue.500" borderRadius="full" w="100%" h="100%"></Box>}
-            />
+            <Box
+              w="100%"
+              h="100%"
+              borderRadius="full"
+              bg={stepColor(index)}
+            ></Box>
           </StepIndicator>
 
           <Box flexShrink="0">
--- src/features/interview.ts
+++ src/features/interview.ts
@@ -1,14 +1,18 @@
 import { create } from "zustand"
+import { immer } from "zustand/middleware/immer"
 
 type Interview = {
   progress: {
     steps: string[] // the steps that can be filled
     currentStep: number // the step that should logically be pending to be filled
-    visibleStep: number // the step that is currently active and visible
+    visibleStep: number // the step that is currently visible
 
-    advanceCurrentStep: () => void // advance the current step to the next, the current step should only move forward
-    gotoNextVisibleStep: () => void
-    gotoPreviousVisibleStep: () => void
+    incrementCurrentStep: () => void
+    decrementCurrentStep: () => void
+    setCurrentStep: (step: number) => void
+
+    incrementVisibleStep: () => void
+    decrementVisibleStep: () => void
     setVisibleStep: (step: number) => void
   }
   data: {
@@ -28,18 +32,39 @@ type Interview = {
   }
 }
 
-const useInterview = create<Interview>()((set, get) => ({
+const useInterview = create<Interview>()(immer((set, get) => ({
   progress: {
     steps: ["Eligibility", "Finances", "Assets", "Children", "Harm", "Proceed"],
     currentStep: 0,
     visibleStep: 0,
 
-    advanceCurrentStep: () => set((state) => ({ progress: { ...state.progress, currentStep: state.progress.currentStep + 1, visibleStep: state.progress.currentStep + 1 } })),
-    gotoNextVisibleStep: () => set((state) => ({ progress: { ...state.progress, visibleStep: state.progress.currentStep + 1 } })),
-    gotoPreviousVisibleStep: () => set((state) => ({ progress: { ...state.progress, visibleStep: state.progress.currentStep - 1 } })),
-    setVisibleStep: (step) => set((state) => {
-      if (step > state.progress.currentStep) return state
-      return { ...state, progress: { ...state.progress, visibleStep: step } }
+    incrementCurrentStep: () => set(state => {
+      state.progress.currentStep = state.progress.currentStep + 1
+      state.progress.visibleStep = state.progress.currentStep
+    }),
+
+    decrementCurrentStep: () => set(state => {
+      state.progress.currentStep = state.progress.currentStep - 1
+      state.progress.visibleStep = state.progress.currentStep
+    }),
+
+    setCurrentStep: step => set(state => {
+      state.progress.currentStep = step
+      state.progress.visibleStep = step
+    }),
+
+    incrementVisibleStep: () => set(state => {
+      state.progress.visibleStep = state.progress.currentStep + 1
+    }),
+
+    decrementVisibleStep: () => set(state => {
+      state.progress.visibleStep = state.progress.currentStep - 1
+    }),
+
+    setVisibleStep: step => set(state => {
+      // user cannot display a step that comes after the current step
+      if (step > state.progress.currentStep) return
+      state.progress.visibleStep = step
     }),
   },
   data: {
@@ -50,13 +75,30 @@ const useInterview = create<Interview>()((set, get) => ({
     agreeOnDivisionOfDepts: undefined,
     agreeOnFinancialSupport: undefined,
 
-    setLivedInNevadaForAtLeast6Weeks: (value) => set((state) => ({ data: { ...state.data, livedInNevadaForAtLeast6Weeks: value } })),
-    setPropertyOrAssetsToDivide: (value) => set((state) => ({ data: { ...state.data, propertyOrAssetsToDivide: value } })),
-    setAgreeOnHowToDivideProperty: (value) => set((state) => ({ data: { ...state.data, agreeOnHowToDivideProperty: value } })),
-    setAgreeOnDivisionOfAssets: (value) => set((state) => ({ data: { ...state.data, agreeOnDivisionOfAssets: value } })),
-    setAgreeOnDivisionOfDepts: (value) => set((state) => ({ data: { ...state.data, agreeOnDivisionOfDepts: value } })),
-    setAgreeOnFinancialSupport: (value) => set((state) => ({ data: { ...state.data, agreeOnFinancialSupport: value } })),
+    setLivedInNevadaForAtLeast6Weeks: value => set(state => {
+      state.data.livedInNevadaForAtLeast6Weeks = value
+    }),
+
+    setPropertyOrAssetsToDivide: value => set(state => {
+      state.data.propertyOrAssetsToDivide = value
+    }),
+
+    setAgreeOnHowToDivideProperty: value => set(state => {
+      state.data.agreeOnHowToDivideProperty = value
+    }),
+
+    setAgreeOnDivisionOfAssets: value => set(state => {
+      state.data.agreeOnDivisionOfAssets = value
+    }),
+
+    setAgreeOnDivisionOfDepts: value => set(state => {
+      state.data.agreeOnDivisionOfDepts = value
+    }),
+
+    setAgreeOnFinancialSupport: value => set(state => {
+      state.data.agreeOnFinancialSupport = value
+    }),
   },
-}))
+})))
 
 export default useInterview
